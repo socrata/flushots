034e9cccc09c2688521b98279a70141cc62f56db
Reid Serozi <reid.serozi@socrata.com>
2014-10-27 14:22:51 -0400

    Changes to port from using Google Fusion Tables to the Socrata APIs.

--- htdocs/ical.php
+++ htdocs/ical.php
@@ -3,7 +3,7 @@
  * Echo an iCalendar for jquery.icalendar.js
 *
 * Written by Keith Wood (kbwood{at}iinet.com.au) July 2008
-* Altered by Tom Kompare (tom@kompare.us) September 2012
+* Altered by Tom Kompare (tom@kompare.us) July 2013
 */
 ini_set("display_errors", 1);
 error_reporting(E_ALL & ~E_NOTICE);
@@ -14,7 +14,7 @@ if($_SERVER['SERVER_NAME'] == $hostArray[0])
 	$content = $_GET["content"];
 	header("Content-type: text/calendar");
 	header("Content-length: ".strlen($content));
-	header("Content-disposition: attachment; filename=flushot.ics");
+	header("Content-disposition: attachment; filename=reminder.ics");
 	echo $content;
 }
 else
